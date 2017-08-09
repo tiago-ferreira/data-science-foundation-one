708d4bb7fc6fb899442c04b215055572963f949f
tiago <si.tiagoferreira@gmail.com>
2017-08-08 23:34:57 -0300

    name and a for to usage string

---
+++ lesson3/striings.py
@@ -0,0 +1,10 @@
+word = 'giripoca'
+print(word[3])
+print(word[3:5])
+print(word[:3])
+print(word[3:])
+print(word[:])
+
+# I can multiply a string with a integer
+print("!" * 12)
+# "" I cannot add a int to string, it's cause a error
