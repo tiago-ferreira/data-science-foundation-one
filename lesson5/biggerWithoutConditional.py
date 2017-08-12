742bd70c4e101077e962978a2575abb27a840099
tiago <si.tiagoferreira@gmail.com>
2017-08-12 13:03:14 -0300

    max and min without conditional operators

--- lesson5/biggerWithoutConditional.py
+++ lesson5/biggerWithoutConditional.py
@@ -4,8 +4,29 @@ def abs(n):
 def bigger(a, b):
     return ((a+b) +  abs(a-b) ) / 2;
 
+def min(x,y):
+    return y ^ ((x ^ y) & -(x < y))
+
+def max(x,y):
+    return x ^ ((x ^ y) & -(x < y))
+
+print('use bigger functon')
 print(bigger(10,7))
 print(bigger(10,17))
 print(bigger(-10,-7))
 print(bigger(-10,-17))
 print(bigger(10,10))
+
+print('use min functon')
+print(min(10,7))
+print(min(10,17))
+print(min(-10,-7))
+print(min(-10,-17))
+print(min(10,10))
+
+print("use max function")
+print(max(10,7))
+print(max(10,17))
+print(max(-10,-7))
+print(max(-10,-17))
+print(max(10,10))
