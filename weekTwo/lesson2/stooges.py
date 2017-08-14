ae700135b8c6d4e9b3febe044a86a8b821b801a9
tiago <si.tiagoferreira@gmail.com>
2017-08-13 23:52:00 -0300

    stooges

---
+++ weekTwo/lesson2/stooges.py
@@ -0,0 +1,19 @@
+# We defined:
+
+stooges = ['Moe','Larry','Curly']
+
+# but in some Stooges films, Curly was
+# replaced by Shemp.
+
+# Write one line of code that changes
+# the value of stooges to be:
+
+#['Moe','Larry','Shemp']
+
+# but does not create a new List
+# object.
+print(stooges)
+
+stooges[2] = 'Shemp'
+
+print(stooges)
