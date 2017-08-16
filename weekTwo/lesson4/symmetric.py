3fe9052974508b77b8463769e11747966f61d03f
tiago <si.tiagoferreira@gmail.com>
2017-08-16 15:06:43 -0300

    adjust symmetric

--- weekTwo/lesson4/symmetric.py
+++ weekTwo/lesson4/symmetric.py
@@ -24,34 +24,34 @@ def symmetric(p):
         return False 
     
 
-print symmetric([[1, 2, 3],
+print(symmetric([[1, 2, 3],
                 [2, 3, 4],
-                [3, 4, 1]])
+                [3, 4, 1]]))
 #>>> True
 
-print symmetric([["cat", "dog", "fish"],
+print(symmetric([["cat", "dog", "fish"],
                  ["dog", "dog", "fish"],
-                 ["fish", "fish", "cat"]])
+                 ["fish", "fish", "cat"]]))
 #>>> True
 
-print symmetric([["cat", "dog", "fish"],
+print(symmetric([["cat", "dog", "fish"],
                  ["dog", "dog", "dog"],
-                 ["fish","fish","cat"]])
+                 ["fish","fish","cat"]]))
 #>>> False
 
-print symmetric([[1, 2],
-                 [2, 1]])
+print(symmetric([[1, 2],
+                 [2, 1]]))
 #>>> True
 
-print symmetric([[1, 2, 3, 4],
+print(symmetric([[1, 2, 3, 4],
                  [2, 3, 4, 5],
-                 [3, 4, 5, 6]])
+                 [3, 4, 5, 6]]))
 #>>> False
 
-print symmetric([[1,2,3],
-                  [2,3,1]])
+print(symmetric([[1,2,3],
+                  [2,3,1]]))
 #>>> False
 
-print symmetric([])
+print(symmetric([]))
 
-print symmetric([[1]])
+print(symmetric([[1]]))
