81aeb4764a5a7dd21559211a85c44893ae329d4d
tiago <si.tiagoferreira@gmail.com>
2017-08-23 20:57:47 -0300

    default arguments mutables

--- weekTwo/lesson5/defaultArguments.py
+++ weekTwo/lesson5/defaultArguments.py
@@ -14,5 +14,19 @@ def print_list(l, numbered = False, bullet_character=' - '):
             print("{} {}".format(bullet_character, element))
 
 
-print_list(["cats", "in", "space"])
-print_list(["cats", "in", "space"], True)
+#print_list(["cats", "in", "space"])
+#print_list(["cats", "in", "space"], True)
+
+
+def todo_list(new_task, base_list=['wake up']):
+    base_list.append(new_task)
+    return base_list
+
+print(todo_list("check the mail"))
+print(todo_list("begin orbital transfer"))
+
+# Está certo! O objeto de lista base_list é criado somente uma vez: 
+# quando a função todo_list é definida. As listas são objetos mutáveis. 
+# Este objeto de lista é usado sempre que a função é chamada, 
+# não é redefinido cada vez que a função é chamada. 
+# Porque todo_list anexa um item à lista, base_list pode ficar mais longo cada vez que todo_list é chamado.
